405ec0d5c6e3842b8dbb592d98ef63394abdbbc1
Varun Venkatesh <vaarunven@gmail.com>
2022-01-26 21:17:01 -0800

    Created hw3 files

---
+++ hw3/hw3.ml
@@ -0,0 +1,78 @@
+(* NOTE: Uncomment the following line if you want to #use this file in utop
+ * (optionally, call it from utop directly): *)
+(* #mod_use "hw3types.ml";; *)
+
+
+(* NOTE: to get rid off the red-wiggles in VSCode, first compile the
+ * the hw3types module running this 
+ * from the command line: 
+       ocamlopt hw3types.ml
+ *)
+open Hw3types
+
+(**** Implement the following functions, remembering the "important note on function bindings" in the assignment write-up ****)
+
+(* #1 *)
+let only_lowercase =
+  failwith "Need to implement only_lowercase"
+
+(* #2 *)
+let longest_string1 =
+  failwith "Need to implement longest_string1"
+
+(* #3 *)
+let longest_string2 =
+  failwith "Need to implement longest_string2"
+
+(* #4 *)
+let longest_string_helper f =
+  failwith "Need to implement longest_string_helper"
+
+let longest_string3 =
+  failwith "Need to implement longest_string3"
+
+let longest_string4 =
+    failwith "Need to implement longest_string4"
+  
+(* #5 *)
+let longest_lowercase =
+  failwith "Need to implement longest_lowercase"
+
+(* #6 *)
+let caps_no_X_string =
+  failwith "Need to implement caps_no_X_string"
+
+(* #7 *)
+let rec first_answer f xs = 
+  failwith "Need to implement first_answer"
+
+(* #8 *)
+let all_answers f xs = 
+  failwith "Need to implement all_answers"
+
+(* #9 *)
+let count_wildcards =
+  failwith "Need to implement count_wildcards"
+
+let count_wild_and_variable_lengths =
+  failwith "Need to implement count_wild_and_variable_lengths"
+
+let count_a_var s = 
+  failwith "Need to implement count_a_var"
+
+(* #10 *)
+let check_pat pat = 
+  failwith "Need to implement check_pat"
+
+(* #11 *)
+let rec matches v pat = 
+  failwith "Need to implement matches"
+
+(* #12 *)
+let first_match v pats = 
+  failwith "Need to implement first_match"
+
+(* optional challenge problem  *)
+
+let typecheck_patterns cons pats = 
+  failwith "Need to implement typecheck_patterns"
