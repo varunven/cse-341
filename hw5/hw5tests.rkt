bb35557fb7ed40bf2d421db4d57e9d76468b73f4
Varun Venkatesh <vaarunven@gmail.com>
2022-02-25 16:01:15 -0800

    Added tests

--- hw5/hw5tests.rkt
+++ hw5/hw5tests.rkt
@@ -13,7 +13,7 @@
   (test-suite
    "Homework 5 Tests"
 
-   (check-equal? (eval-exp (add (int 2) (int 2))) (int 4) "add simple test")
+   (check-equal? (eval-exp (add (int 2) (int 2))) (int 4) "add test")
 
    (check-exn (lambda (x) (string=? (exn-message x) "MUPL addition applied to non-number"))
               (lambda () (eval-exp (add (int 2) (munit))))
@@ -26,86 +26,79 @@
                  (list (int 10) (int 15))
                  "provided combined test using problems 1, 2, and 4")
 
-; Problem 3
-  (check-equal? (eval-exp (add (int 2) (int 2))) (int 4) "Problem3: add simple test")
+; Problem 2
+  (check-equal? (eval-exp (add (int 2) (int 2))) (int 4) "add test")
  
-  (check-equal? (eval-exp (isgreater (int 2) (int 2))) (int 0) "Problem3: isgreater simple test1")
+  (check-equal? (eval-exp (isgreater (int 3) (int 2))) (int 1) "isgreater greater")
+
+  (check-equal? (eval-exp (isgreater (int -4) (int -4))) (int 0) "isgreater equal")
  
-  (check-equal? (eval-exp (isgreater (int 3) (int 2))) (int 1) "Problem3: isgreater simple test2")
+  (check-equal? (eval-exp (isgreater (int 13) (int 18))) (int 0) "isgreater smaller")
  
-  (check-equal? (eval-exp (isgreater (int 2) (int -1))) (int 1) "Problem3: isgreater simple test3")
+  (check-equal? (eval-exp (ifnz (int -2) (int -1) (int -2))) (int -1) "ifnz test1")
  
-  (check-equal? (eval-exp (isgreater (int -2) (int -1))) (int 0) "Problem3: isgreater simple test4")
- 
-  (check-equal? (eval-exp (ifnz (int -2) (int -1) (int -2))) (int -1) "Problem3: ifnz simple test1")
- 
-  (check-equal? (eval-exp (ifnz (int 0) (int -1) (int -2))) (int -2) "Problem3: ifnz simple test2")
+  (check-equal? (eval-exp (ifnz (int 0) (int -1) (int -2))) (int -2) "ifnz test2")
  
   (check-equal? (eval-exp (mlet "foo" (int 1)
-                            (add (var "foo") (int 1)))) (int 2) "Problem3: mlet simple test1")
+                            (add (var "foo") (int 1)))) (int 2) "mlet test1")
 
   (check-equal? (eval-exp (mlet "foo" (int 1)
                            (mlet "bar" (int 2)
-                            (add (var "foo") (var "bar"))))) (int 3) "Problem3: mlet simple test2")
+                            (add (var "foo") (var "bar"))))) (int 3) "mlet test2")
  
-  (check-equal? (eval-exp (first (apair (int 1) (int 2)))) (int 1) "Problem3: apair/first simple test")
+  (check-equal? (eval-exp (first (apair (int 1) (int 2)))) (int 1) "apair/first test")
 
-  (check-equal? (eval-exp (second (apair (int 1) (int 2)))) (int 2) "Problem3: apair/second simple test")
+  (check-equal? (eval-exp (second (apair (int 1) (int 2)))) (int 2) "apair/second test")
 
   (check-equal? (eval-exp
                  (call (eval-exp (fun "plus" "delta" (add (int 1) (var "delta")))) (int 2)))
-                 (int 3) "Problem3: call/fun test1")
+                 (int 3) "call/fun test1")
 
   (check-exn (lambda (x) (string=? (exn-message x) "MUPL addition applied to non-number"))
              (lambda () (eval-exp (add (int 2) (munit))))
-             "Problem3: add bad argument")
+             "add bad argument")
 
-  ; Problem4
+  ; Problem 3
   (check-equal? (eval-exp 
-                 (ifmunit (int 1) (int 2) (int 3))) (int 3) "Problem4: ifmunit simple test1")
+                 (ifmunit (int 1) (int 2) (int 3))) (int 3) "ifmunit test1")
 
   (check-equal? (eval-exp 
-                 (ifmunit (munit) (int 2) (int 3))) (int 2) "Problem4: ifmunit simple test2")
+                 (ifmunit (munit) (int 6) (int 3))) (int 6) "ifmunit test2")
 
   (check-equal? (eval-exp 
                  (ifmunit
                    (second (apair (int 2) (munit)))
                    (int 2)
-                   (int 3))) (int 2) "Problem4: ifmunit simple test3")
+                   (int 3))) (int 2) "ifmunit test3")
 
   (check-equal? (eval-exp 
                  (mlet* (list (cons "a" (int 1)) (cons "b" (int 2)))
-                         (add (var "a") (var "b")))) (int 3) "Problem4: mlet* simple test1")
+                         (add (var "a") (var "b")))) (int 3) "mlet* test1")
 
   (check-equal? (eval-exp 
                  (ifeq (int 1) (int 2) (int 3) (int 4)))
-                 (int 4) "Problem4: ifeq simple test1")
+                 (int 4) "ifeq test1")
 
-  (check-equal? (eval-exp 
-                 (ifeq (int 2) (int 2) (int 3) (int 4)))
-                 (int 3) "Problem4: ifeq simple test2")
-
-  ; Problem5
+  ; Problem 4
   (check-equal? (eval-exp
                   (call (call mupl-filter
                           (fun null "item" (isgreater (var "item") (int 5))))
                         (racketlist->mupllist (list (int 19)))))
                 (racketlist->mupllist (list (int 19)))
-                "Problem5: mulpl-filter simple test1")
+                "mulpl-filter test1")
 
   (check-equal? (eval-exp
                   (call (call mupl-filter
                           (fun null "item" (isgreater (var "item") (int 5))))
-                        (racketlist->mupllist (list (int 1) (int 2) (int 5) (int 10) (int 19)))))
-                (racketlist->mupllist (list (int 10) (int 19)))
-                "Problem5: mulpl-filter simple test2")
+                        (racketlist->mupllist (list (int -1) (int 1) (int 4) (int 8) (int 10) (int 19)))))
+                (racketlist->mupllist (list (int 8) (int 10) (int 19)))
+                "mulpl-filter test2")
 
   (check-equal? (mupllist->racketlist
                  (eval-exp (call (call mupl-all-gt (int 9))
                                  (racketlist->mupllist 
                                   (list (int 10) (int 9) (int 15))))))
-                (list (int 10) (int 15))
-                "provided combined test using problems 2, 3, and 5")
+                (list (int 10) (int 15)) "mupl-all-gt test")
 
   (check-equal? (mupllist->racketlist
        (eval-exp (call (call mupl-mapAddN (int 7))
